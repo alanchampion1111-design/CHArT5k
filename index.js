1f3f6e94751bf7f8d6f19d9ddcba3747f7fa51d8
Alan Champion <alan.champion1111@gmail.com>
2026-02-18 08:10:21 +0100

    Fix caching logic and variable assignment

--- index.js
+++ index.js
@@ -473,13 +473,13 @@ exports.filterUrl = async (req,res) => {
     +'-H "Content-Type: application/json"';
   console.log('Test: '+testCmd);
   var thisPage;
-  if (!caching)  = {};      // clear any cache during batching
-  if (thisUrl in ) {        // typically, many runners at the same event (during weekly import only)
+  if (!caching) cachedPages = {};      // clear any cache during batching
+  if (thisUrl in cachedPages) {        // typically, many runners at the same event (during weekly import only)
     console.log('Re-using detailed results from cached URL, '+thisUrl);
-    thisPage = [thisUrl];    // ...and so no delay in loading OR in awaiting enforced delay between each
+    thisPage = cachedPages[thisUrl];    // ...and so no delay in loading OR in awaiting enforced delay between each
   } else {
     thisPage = await loadUrl(thisUrl,loadSECS,true);
-    if (caching) [thisUrl] = thisPage;
+    if (caching) cachedPages[thisUrl] = thisPage;
   }
   try {  // Get 2 (or more) positions in series?
     // 1. Sort by (descending) Age-Grade, to get ageGrade position of matchRunner
