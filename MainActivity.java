f74dbecbb0c9005be30c26b64f0b2d20e4a4ef1f
Alan Champion <alan.champion1111@gmail.com>
2026-07-14 10:31:54 +0200

    Refactor WebView setup in onCreate method
    Refactored WebView initialization to remove redundancy.

--- MainActivity.java
+++ MainActivity.java
@@ -30,42 +30,40 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Keep alive when rotating mobile 
-        if (myWebView == null) {
-            myWebView = new WebView(this);
-            WebSettings webSettings = myWebView.getSettings();
-            webSettings.setJavaScriptEnabled(true);
-            webSettings.setDomStorageEnabled(true);
-            // Force local cache to remember your active tab/screen changes
-            webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
-            // Core layout mapping options
-            webSettings.setUseWideViewPort(true);       
-            webSettings.setLoadWithOverviewMode(true);  
-            webSettings.setSupportZoom(true);           
-            webSettings.setBuiltInZoomControls(true);
-            webSettings.setDisplayZoomControls(false);
-            myWebView.setImportantForAutofill(View.IMPORTANT_FOR_AUTOFILL_YES);
-            // webSettings.setUserAgentString(DESKTOP_UA); 
-            
-            myWebView.setWebViewClient(new WebViewClient() {
-                @Override
-                public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                    if (url.contains("script.google.com")) {
-                        return false;    // action internal to App
-                    }    // otherwise for mail requests, for links to Group SS, or external links to parkrun 
-                    String actionIntent = url.startsWith("mailto:")
-                        ? Intent.ACTION_SENDTO
-                        : Intent.ACTION_VIEW;// assume  (url.startsWith("mailto:") || url.contains("docs.google.com"))
-                    try {       // handle externally via user's preferred emailer or browser
-                        Intent intent = new Intent(actionIntent, Uri.parse(url));
-                        startActivity(intent);
-                    } catch (Exception e) {}
-                    return true;
-                }
-            });
-            
-            myWebView.loadUrl("https://script.google.com/macros/s/AKfycbzGA2ARs2d8ON4xfIOKTMY5WFqE5oyNz5XLhEB_LeIzqj3mKNJdj2P84upsypi6hf96/exec");
-        }
+
+        myWebView = new WebView(this);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        // Force local cache to remember your active tab/screen changes
+        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        // Core layout mapping options
+        webSettings.setUseWideViewPort(true);       
+        webSettings.setLoadWithOverviewMode(true);  
+        webSettings.setSupportZoom(true);           
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(false);
+        myWebView.setImportantForAutofill(View.IMPORTANT_FOR_AUTOFILL_YES);
+        // webSettings.setUserAgentString(DESKTOP_UA); 
+        
+        myWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.contains("script.google.com")) {
+                    return false;    // action internal to App
+                }    // otherwise for mail requests, for links to Group SS, or external links to parkrun 
+                String actionIntent = url.startsWith("mailto:")
+                    ? Intent.ACTION_SENDTO
+                    : Intent.ACTION_VIEW;// assume  (url.startsWith("mailto:") || url.contains("docs.google.com"))
+                try {       // handle externally via user's preferred emailer or browser
+                    Intent intent = new Intent(actionIntent, Uri.parse(url));
+                    startActivity(intent);
+                } catch (Exception e) {}
+                return true;
+            }
+        });
+        
+        myWebView.loadUrl("https://script.google.com/macros/s/AKfycbzGA2ARs2d8ON4xfIOKTMY5WFqE5oyNz5XLhEB_LeIzqj3mKNJdj2P84upsypi6hf96/exec");
         setContentView(myWebView);
     }
     
